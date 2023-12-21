8480ed97d434efe235f46d15694f5c891f030a52
Otonashi_Kaguya <tsukiianya@gmail.com>
2023-12-21 18:55:14 -0500

    Change to ports

--- go.mod
+++ go.mod
@@ -1,10 +1,3 @@
 module cnls.io
 
 go 1.21.4
-
-require golang.org/x/crypto v0.17.0
-
-require (
-	golang.org/x/net v0.10.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
-)
--- main.go
+++ main.go
@@ -1,64 +1,31 @@
 package main
 
 import (
-	"crypto/tls"
 	"fmt"
-	"golang.org/x/crypto/acme/autocert"
-	"net"
 	"net/http"
 )
 
 func main() {
-	// Specify the domain for which you want to get a TLS certificate
-	domain := "cnls.io"
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		html := `
+			<!DOCTYPE html>
+			<html>
+			<head>
+				<title>Welcome to My Go Web Page</title>
+			</head>
+			<body>
+				<h1>Hello from Go!</h1>
+				<p>This is a basic HTML page served by a Go program.</p>
+			</body>
+			</html>
+		`
+		fmt.Fprint(w, html)
+	})
 
-	// Setup automatic certificate management with Let's Encrypt via Cloudflare DNS
-	m := autocert.Manager{
-		Prompt:     autocert.AcceptTOS,
-		HostPolicy: autocert.HostWhitelist(domain),
-		Cache:      autocert.DirCache("certs"), // Store certificates in a cache directory
-	}
-
-	// Create a TCP listener on a specific IPv4 address and port 443
-	listener, err := net.Listen("tcp4", ":443")
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-
-	// Create a new HTTP server
-	server := &http.Server{
-		Addr:    ":443",
-		Handler: http.HandlerFunc(handler),
-		TLSConfig: &tls.Config{
-			GetCertificate: m.GetCertificate,
-		},
-	}
-
-	// Print a message indicating the server is running
-	fmt.Printf("Server is running on https://%s\n", domain)
-
-	// Start the HTTPS server
-	err = server.ServeTLS(listener, "", "")
+	port := 8080
+	fmt.Printf("Starting server on :%d...\n", port)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 	if err != nil {
 		fmt.Println("Error:", err)
 	}
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	// Your HTML content here
-	html := `
-		<!DOCTYPE html>
-		<html>
-		<head>
-			<title>Go Web Server</title>
-		</head>
-		<body>
-			<h1>Hello, this is your Go web server!</h1>
-		</body>
-		</html>
-	`
-
-	// Write the HTML content to the response writer
-	w.Write([]byte(html))
 }
